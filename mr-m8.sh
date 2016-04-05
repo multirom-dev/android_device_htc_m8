92db310e8fa0827dac23bfe02668d57cfcf73613
jenkins <jenkins@shadow-projects.root.sx>
2016-04-05 09:35:38 -0400

    Remove repo sync from script
    - Messes up checked out branch is using prebuilt kernel

--- mr-m8.sh
+++ mr-m8.sh
@@ -21,4 +21,4 @@
 NUMCORES=`getconf _NPROCESSORS_ONLN`
 JSYNC=`echo $(( $NUMCORES * 2 ))`
 
-repo sync -j$JSYNC && . build/envsetup.sh && lunch omni_m8-eng && make -j$JSYNC recoveryimage && make -j$JSYNC multirom_zip && make -j$JSYNC multirom_uninstaller
+. build/envsetup.sh && lunch omni_m8-eng && make -j$JSYNC recoveryimage && make -j$JSYNC multirom_zip && make -j$JSYNC multirom_uninstaller
